669c838a1c6a301aef43d0541f6dd2645c6c3f0a
kota-yata <kota@yatagai.com>
2020-12-18 06:00:48 +0900

    add router and definition.scss

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script lang="ts">
-  export let name: string;
+  import Router from "svelte-spa-router";
+  import Home from "./routes/Home.svelte";
+  import SignUp from "./routes/SignUp.svelte";
+  import SignIn from "./routes/SignIn.svelte";
+
+  const routes = {
+    "/": Home,
+    "/signup/": SignUp,
+    "/signin/": SignIn,
+  };
 </script>
 
 <main>
-  <h1>{name}</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Router routes="{routes}" />
 </main>
-
-<style lang="scss">
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
