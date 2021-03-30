c6938e15bb3ec338e0a90636b5d707cfd49baeb3
kota-yata <kota@yatagai.com>
2021-03-31 04:19:53 +0900

    setting terms hyperlink instead of setting help page #9

--- src/components/Copyright.svelte
+++ src/components/Copyright.svelte
@@ -1,17 +1,32 @@
-<div class="rightcard-copyright"><span>© 2021 Kota Yatagai</span></div>
+<script lang="ts">
+  import { link } from "svelte-spa-router";
+</script>
+
+<div class="rightcard-copyright">
+  <div class="terms">
+    <a href="/privacy-policy" use:link>プライバシーポリシー</a>
+    ・<a href="/term-of-use" use:link>利用規約</a>
+  </div>
+  <div>© 2021 Kota Yatagai</div>
+</div>
 
 <style lang="scss">
   @import "../assets/definition.scss";
 
   .rightcard-copyright {
+    @extend %center;
     position: absolute;
     left: 50%;
     transform: translateX(-50%);
     bottom: 10px;
-    span {
+    div {
       color: $slouch-green;
       font-weight: bold;
       font-size: 15px;
+    }
+    .terms {
+      font-size: 10px;
+      margin-bottom: 5px;
     }
   }
 </style>
